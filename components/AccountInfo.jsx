4af394d5a37fd47289f025424ef673824529a847
alonpeled44 <alon.orli44@gmail.com>
2024-03-28 14:12:05 +0300

    Connect style file, seperate user info

--- components/AccountInfo.jsx
+++ components/AccountInfo.jsx
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import styles from "@/styles/Home.module.scss";
+
 export default function AccountInfo(props) {
 	return (
-		<>
-			<img src={props.src} />
-			<p>{props.fullName}</p>
-			<p>|</p>
-			<p>Day {props.day}</p>
-		</>
+		<div className={props.className}>
+			<img src={props.src} className={styles.img} />
+			<div className={styles.div}>
+				<p className={styles.p}>{props.fullName}</p>
+				<p className={styles.p}>|</p>
+				<div className={styles.div}>
+					<p className={styles.p}>Day</p>
+					<p className={styles.date}>{props.day}</p>
+				</div>
+			</div>
+		</div>
 	);
 }
