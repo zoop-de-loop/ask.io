751608ad7c70b78b6e35f219367f7acaa6a43299
alonpeled44 <alon.orli44@gmail.com>
2024-03-28 14:09:42 +0300

    make buttons clickable, add circles and connect to style file

--- components/LinkCard.jsx
+++ components/LinkCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import styles from "@/styles/Home.module.scss";
 
 export default function LinkCard(props) {
 	return (
-		<div>
-			<h2>{props.name}</h2>
-			{props.icon}
-		</div>
+		<a className={props.className} href={`#${props.name}`}>
+			<h2 className={styles.h2}>{props.name}</h2>
+
+			<div className={styles.circleContainer}>
+				<div className={styles.bigCircle}>
+					<div className={styles.svg}>{props.icon}</div>
+				</div>
+				<div className={styles.mediumCircle}></div>
+				<div className={styles.smallCircle}></div>
+			</div>
+		</a>
 	);
 }
