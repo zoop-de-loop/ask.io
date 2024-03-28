bc6145b7899dccc27f03a3f3347f5245a733a58f
alonpeled44 <alon.orli44@gmail.com>
2024-03-28 14:17:53 +0300

    Add static img, connect style file

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,19 +4,20 @@ import MyProfileIcon from "@/components/MyProfileIcon";
 import SettingsIcon from "@/components/SettingsIcon";
 import LinkCard from "@/components/LinkCard";
 import AccountInfo from "@/components/AccountInfo";
+import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
 	return (
 		<>
-			<header>
-				<AccountInfo src='' fullName='Lio Giladi' day='9' />
-				<h1>Ask.io</h1>
+			<header className={styles.header}>
+				<AccountInfo src={"/lio_img.png"} fullName='Lio Giladi' day='9' className={styles.accountInfo} />
+				<h1 className={styles.h1}>Ask.io</h1>
 			</header>
-			<main>
-				<LinkCard name='Asks' icon={<AsksIcon />} />
-				<LinkCard name='Statistics' icon={<StatisticsIcon />} />
-				<LinkCard name='My Profile' icon={<MyProfileIcon />} />
-				<LinkCard name='Settings' icon={<SettingsIcon />} />
+			<main className={styles.main}>
+				<LinkCard name='Asks' icon={<AsksIcon />} className={styles.LinkCard} />
+				<LinkCard name='Statistics' icon={<StatisticsIcon />} className={styles.LinkCard} />
+				<LinkCard name='My Profile' icon={<MyProfileIcon />} className={styles.LinkCard} />
+				<LinkCard name='Settings' icon={<SettingsIcon />} className={styles.LinkCard} />
 			</main>
 		</>
 	);
