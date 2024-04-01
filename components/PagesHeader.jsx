f9665e976f99d8cbb9baa8c21f3dcff59a1e35ee
alonpeled44 <alon.orli44@gmail.com>
2024-04-01 15:57:50 +0300

    Link style file + fix home url

--- components/PagesHeader.jsx
+++ components/PagesHeader.jsx
@@ -1,11 +1,11 @@
 import AccountInfo from "@/components/AccountInfo";
-
+import styles from "@/styles/components/pages-header.module.scss";
 export default function PagesHeader() {
 	return (
-		<header>
+		<header id={styles.header}>
 			<AccountInfo profilePictureSrc={"/lio_img.png"} fullName='Lio Giladi' daysSinceRegistration={9} />
 			<h1>Ask.io</h1>
-			<a href='/index'>HOME</a>
+			<a href='/'>HOME</a>
 		</header>
 	);
 }
