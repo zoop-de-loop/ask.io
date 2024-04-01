c9c40b2ff60f954ebd475e09c498bb55486611d4
alonpeled44 <alon.orli44@gmail.com>
2024-04-01 16:42:35 +0300

    Add statistics to page

--- pages/statistics.jsx
+++ pages/statistics.jsx
@@ -1,10 +1,16 @@
 import PagesHeader from "@/components/PagesHeader";
-
+import Statistic from "@/components/statistic";
+import styles from "@/styles/pages/statistics.module.scss";
 export default function Statistics() {
 	return (
 		<>
 			<PagesHeader />
-			<main></main>
+			<main className={styles.main}>
+				<Statistic amount={19} statisticName='all asks' />
+				<Statistic amount={7} statisticName='my asks' />
+				<Statistic amount={3} statisticName='liked asks' />
+				<Statistic amount={1} statisticName='accounts' />
+			</main>
 		</>
 	);
 }
