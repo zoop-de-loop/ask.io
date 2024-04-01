8efc5eeede1f1234a1f88342c84402a25625b5a8
alonpeled44 <alon.orli44@gmail.com>
2024-04-01 16:23:36 +0300

    Make statistic component skeleton + use props

--- components/statistic.jsx
+++ components/statistic.jsx
@@ -1,5 +1,10 @@
 import styles from "@/styles/components/statistic.module.scss";
 
-export default function Statistic() {
-	return <></>;
+export default function Statistic({ amount, statisticName }) {
+	return (
+		<article className={styles.article}>
+			<p>{amount}</p>
+			<p>{statisticName}</p>
+		</article>
+	);
 }
