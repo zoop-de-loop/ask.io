ebe13b769f8c5e321170aa1c4367b17badc10f9f
alonpeled44 <alon.orli44@gmail.com>
2024-04-02 16:37:50 +0300

    Move data in statistics.jsx to be out of the component

--- pages/statistics.jsx
+++ pages/statistics.jsx
@@ -2,17 +2,17 @@ import Header from "@/components/Header";
 import Stat from "@/components/Stat";
 import styles from "@/styles/pages/statistics.module.scss";
 
-export default function Statistics() {
-	const data = [
-		{ amount: 19, name: "all asks" },
-		{ amount: 7, name: "my asks" },
-		{ amount: 3, name: "liked asks" },
-		{ amount: 3, name: "accounts" },
-		{ amount: 1, name: "accounts" },
-		{ amount: 1, name: "accounts" },
-		{ amount: 1, name: "accounts" },
-	];
+const data = [
+	{ amount: 19, name: "all asks" },
+	{ amount: 7, name: "my asks" },
+	{ amount: 3, name: "liked asks" },
+	{ amount: 3, name: "accounts" },
+	{ amount: 1, name: "accounts" },
+	{ amount: 1, name: "accounts" },
+	{ amount: 1, name: "accounts" },
+];
 
+export default function Statistics() {
 	const stats = data.map(({ amount, name }) => <Stat amount={amount} name={name} />);
 
 	return (
