796a930e4950e585e17e343597c96acd4f7968f1
alonpeled44 <alon.orli44@gmail.com>
2024-04-03 13:58:48 +0300

    Add users data + relocate asksData to a seperate file

--- pages/asks.jsx
+++ pages/asks.jsx
@@ -1,31 +1,17 @@
 import styles from "@/styles/pages/asks.module.scss";
 import Ask from "@/components/Ask";
+import asksData from "@/server/asksData";
 
-const asks_data = [
-	{ id: 0, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 1, ask: "Can a dog love me?", like: true, anonymous: true },
-	{ id: 2, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 3, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 4, ask: "Can a dog love me?", like: true, anonymous: true },
-	{ id: 5, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 6, ask: "Can a dog love me?", like: true, anonymous: false },
-	{ id: 7, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 8, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 9, ask: "Can a dog love me?", like: false, anonymous: false },
-	{ id: 10, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 11, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 12, ask: "Can a dog love me?", like: false, anonymous: true },
-	{ id: 13, ask: "Can a dog love me?", like: false, anonymous: true },
-];
 export default function Asks() {
-	const show_ask = asks_data.map(({ ask, like, anonymous }) => {
-		<Ask ask={ask} like={like} anonymous={anonymous} />;
+	const showAsk = asksData.map(({ ask, like, anonymous }) => {
+		return <Ask ask={ask} like={like} anonymous={anonymous} />;
 	});
+	console.log(showAsk);
 
 	return (
 		<main>
 			<div className={styles["asks-and-buttons"]}>
-				<div className={styles.asks}>{show_ask}</div>
+				<div className={styles.asks}>{showAsk}</div>
 				<div className={styles.buttons}>
 					<button>Show liked Asks</button>
 					<button>Ask</button>
---
+++ server/asksData.js
@@ -0,0 +1,18 @@
+const asksData = [
+	{ id: 0, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 1, ask: "Can a dog love me?", like: true, anonymous: true },
+	{ id: 2, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 3, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 4, ask: "Can a dog love me?", like: true, anonymous: true },
+	{ id: 5, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 6, ask: "Can a dog love me?", like: true, anonymous: false },
+	{ id: 7, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 8, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 9, ask: "Can a dog love me?", like: false, anonymous: false },
+	{ id: 10, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 11, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 12, ask: "Can a dog love me?", like: false, anonymous: true },
+	{ id: 13, ask: "Can a dog love me?", like: false, anonymous: true },
+];
+
+export default asksData;
