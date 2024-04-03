6e3d896df6559de4e17f8409c8260a6000eed59e
alonpeled44 <alon.orli44@gmail.com>
2024-04-03 10:04:07 +0300

    Change homeLink rendering to be in one place

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,18 @@
 import AccountInfo from "@/components/AccountInfo";
 import styles from "@/styles/components/header.module.scss";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Header() {
 	const router = useRouter();
-	return router.asPath === "/" /* home page, not supposed to render home link */ ? (
-		<header id={styles.header}>
-			<AccountInfo profilePictureSrc={"/lio_img.png"} fullName='Lio Giladi' daysSinceRegistration={9} />
-			<h1>Ask.io</h1>
-		</header>
-	) : (
-		/* other pages, supposed to render home link */
-		<header id={styles.header}>
-			<AccountInfo profilePictureSrc={"/lio_img.png"} fullName='Lio Giladi' daysSinceRegistration={9} />
-			<h1>Ask.io</h1>
-			<a href='/'>HOME</a>
-		</header>
-	);
+	const homeLink =
+		router.asPath === "/" ? undefined : <a href='/'>HOME</a>; /* if homepage homeLink will be undefined so it wont be rendered */
 
 	return (
 		<header id={styles.header}>
 			<AccountInfo profilePictureSrc={"/lio_img.png"} fullName='Lio Giladi' daysSinceRegistration={9} />
 			<h1>Ask.io</h1>
-			<a href='/'>HOME</a>
+			{homeLink}
 		</header>
 	);
 }
