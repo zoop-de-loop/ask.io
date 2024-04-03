bb021835e33e886e0113fd563b1cd08b400f3e13
alonpeled44 <alon.orli44@gmail.com>
2024-04-03 09:38:48 +0300

    Change home link to only render when needed, not only show

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,17 +4,25 @@ import { useRouter } from "next/router";
 
 export default function Header() {
 	const router = useRouter();
-	const style = {
-		display: router.asPath === "/" ? "none" : "block",
-	};
+	return router.asPath === "/" /* home page, not supposed to render home link */ ? (
+		<header id={styles.header}>
+			<AccountInfo profilePictureSrc={"/lio_img.png"} fullName='Lio Giladi' daysSinceRegistration={9} />
+			<h1>Ask.io</h1>
+		</header>
+	) : (
+		/* other pages, supposed to render home link */
+		<header id={styles.header}>
+			<AccountInfo profilePictureSrc={"/lio_img.png"} fullName='Lio Giladi' daysSinceRegistration={9} />
+			<h1>Ask.io</h1>
+			<a href='/'>HOME</a>
+		</header>
+	);
 
 	return (
 		<header id={styles.header}>
 			<AccountInfo profilePictureSrc={"/lio_img.png"} fullName='Lio Giladi' daysSinceRegistration={9} />
 			<h1>Ask.io</h1>
-			<a href='/' style={style}>
-				HOME
-			</a>
+			<a href='/'>HOME</a>
 		</header>
 	);
 }
