cf1b128ee9cc7a905e16e79148a45a909a270243
alonpeled44 <alon.orli44@gmail.com>
2024-04-03 10:23:51 +0300

    Change home link rendering to use the && operator

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,13 +5,12 @@ import { useRouter } from "next/router";
 
 export default function Header() {
 	const router = useRouter();
-	const homeLink = router.asPath === "/" ? undefined : <a href='/'>HOME</a>;
 
 	return (
 		<header id={styles.header}>
 			<AccountInfo profilePictureSrc={"/lio_img.png"} fullName='Lio Giladi' daysSinceRegistration={9} />
 			<h1>Ask.io</h1>
-			{homeLink}
+			{router.asPath != "/" && <a href='/'>HOME</a>}
 		</header>
 	);
 }
