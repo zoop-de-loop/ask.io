1a73563403f45c5e0ffef6e17f093cdf974a8270
alonpeled44 <alon.orli44@gmail.com>
2024-04-04 09:45:58 +0300

    Make ShowLikedAsks button toggleable + showing the asks respectfully

--- pages/asks.jsx
+++ pages/asks.jsx
@@ -1,19 +1,38 @@
 import styles from "@/styles/pages/asks.module.scss";
 import Ask from "@/components/Ask";
 import asksData from "@/server/asksData";
+import React, { useState } from "react";
 
 export default function Asks() {
 	const askDataArr = Object.entries(asksData);
-	const showAsk = askDataArr.map((dataArr) => {
-		return <Ask id={dataArr[0]} ask={dataArr[1]["ask"]} like={dataArr[1]["like"]} anonymous={dataArr[1]["anonymous"]} />;
-	});
+	const [likedActive, setLikedActive] = useState(false);
+
+	const handleLikeButtonClick = () => {
+		setLikedActive((prev) => {
+			return !prev;
+		});
+	};
+	let showAsk = [];
+
+	if (likedActive) {
+		const likedAsks = askDataArr.filter((dataArr) => dataArr[1]["like"] === true);
+		console.log(likedAsks);
+		showAsk = likedAsks.map((dataArr) => {
+			console.log(`==> ask: ${dataArr[1]["ask"]} like: ${dataArr[1]["like"]} anonymous: ${dataArr[1]["anonymous"]}`);
+			return <Ask key={dataArr[0]} ask={dataArr[1]["ask"]} like={dataArr[1]["like"]} anonymous={dataArr[1]["anonymous"]} />;
+		});
+	} else {
+		showAsk = askDataArr.map((dataArr) => {
+			return <Ask key={dataArr[0]} ask={dataArr[1]["ask"]} like={dataArr[1]["like"]} anonymous={dataArr[1]["anonymous"]} />;
+		});
+	}
 
 	return (
 		<main>
 			<div className={styles["asks-and-buttons"]}>
 				<div className={styles.asks}>{showAsk}</div>
 				<div className={styles.buttons}>
-					<button>Show liked Asks</button>
+					<button onClick={handleLikeButtonClick}>{likedActive ? "Show All Asks" : "Show liked Asks"}</button>
 					<button>Ask</button>
 				</div>
 			</div>
