f4ab6f09bf57250c91c60a303ab848dfb0e251dd
alonpeled44 <alon.orli44@gmail.com>
2024-04-11 13:54:28 +0300

    Changing Modal from div to dialog

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,8 +1,14 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import styles from "@/styles/components/modal.module.scss";
 
 export default function Modal({ content, isModalOpen, setIsModalOpen }) {
 	const modalRef = useRef(null);
+
+	useEffect(() => {
+		if (isModalOpen) {
+			modalRef.current.showModal();
+		}
+	}, [isModalOpen]);
 
 	const backgroundClicked = (event) => {
 		if (event.target === modalRef.current) {
@@ -10,11 +16,19 @@ export default function Modal({ content, isModalOpen, setIsModalOpen }) {
 		}
 	};
 
-	if (!isModalOpen) return;
+	const buttonClicked = (event) => {
+		if (event.key === "Escape") {
+			setIsModalOpen(false);
+		}
+	};
+
+	if (!isModalOpen) {
+		return;
+	}
 
 	return (
-		<div className={styles.modal} onClick={backgroundClicked} ref={modalRef}>
+		<dialog className={styles.modal} onClick={backgroundClicked} ref={modalRef} onKeyDown={buttonClicked}>
 			{content}
-		</div>
+		</dialog>
 	);
 }
