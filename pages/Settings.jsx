43f7cdd3791d9697a958cd46ecc623154a1b3ed4
alonpeled44 <alon.orli44@gmail.com>
2024-04-14 16:44:09 +0300

    Add test button for color change

--- pages/Settings.jsx
+++ pages/Settings.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 import styles from "@/styles/pages/settings.module.scss";
 
 export default function Settings() {
-	return <></>;
+	const [colorMode, setColorMode] = useState("bright");
+	const toggleColorMode = () => {
+		setColorMode((prev) => {
+			document.documentElement.style.setProperty("--primary-color", prev !== "bright" ? "#EC7373" : "#B95151");
+			document.documentElement.style.setProperty("--secondary-color", prev !== "bright" ? "#FFFFFF" : "#301E1E");
+			document.documentElement.style.setProperty("--text-color", prev !== "bright" ? "#000000" : "#FFFFFF");
+			return prev === "bright" ? "dark" : "bright";
+		});
+	};
+	return (
+		<main>
+			<button onClick={toggleColorMode}>PRESS ME</button>
+		</main>
+	);
 }
