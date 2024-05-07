b03616628fec34ec02c26f1b54df64aacee3051e
alonpeled44 <alon.orli44@gmail.com>
2024-05-07 12:17:54 +0300

    Add changing pictures and only support image files

--- pages/account.jsx
+++ pages/account.jsx
@@ -4,6 +4,7 @@ export default function Account() {
 	const daysAlive = 9;
 	const [isEditActive, setIsEditActive] = useState(false);
 	const [userName, setUserName] = useState("Lio Giladi");
+	const [profilePicSrc, setProfilePicSrc] = useState("/lio_img.png");
 	const [changingUserName, setChangingUsername] = useState(userName);
 
 	const onEditbuttonClick = () => {
@@ -20,13 +21,37 @@ export default function Account() {
 		setChangingUsername(event.target.value);
 	};
 
+	const onUpload = (event) => {
+		try {
+			const file = event.target.files[0];
+
+			let isImage = false;
+			switch (file["type"]) {
+				case "image/jpeg":
+					isImage = true;
+					break;
+				case "image/png":
+					isImage = true;
+					break;
+				case "image/jpg":
+					isImage = true;
+					break;
+				default:
+					isImage = false;
+					break;
+			}
+
+			isImage ? setProfilePicSrc(URL.createObjectURL(file)) : alert("file not supported");
+		} catch {}
+	};
+
 	return (
 		<main id={styles.main}>
 			<label for='profilePicInput' className={styles["profile-pic-input"]}>
-				<img src='/lio_img.png' alt="user's photo" />
+				<img src={profilePicSrc} alt="user's photo" />
 			</label>
 
-			<input type='file' id='profilePicInput' />
+			<input type='file' id='profilePicInput' onChange={onUpload} />
 
 			<section>
 				<input
