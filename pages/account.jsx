5b9fe6c7830c543a10e154d9b632fab94c3396da
alonpeled44 <alon.orli44@gmail.com>
2024-05-07 11:41:05 +0300

    Enabling username change and saving

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,6 +1,24 @@
 import styles from "@/styles/pages/account.module.scss";
+import { useState } from "react";
 export default function Account() {
 	const daysAlive = 9;
+	const [isEditActive, setIsEditActive] = useState(false);
+	const [userName, setUserName] = useState("Lio Giladi");
+	const [changingUserName, setChangingUsername] = useState(userName);
+
+	const onEditbuttonClick = () => {
+		setIsEditActive((prev) => {
+			if (prev === true) {
+				setUserName(changingUserName);
+			}
+
+			return !prev;
+		});
+	};
+
+	const onUserNameChange = (event) => {
+		setChangingUsername(event.target.value);
+	};
 
 	return (
 		<main id={styles.main}>
@@ -11,8 +29,16 @@ export default function Account() {
 			<input type='file' id='profilePicInput' />
 
 			<section>
-				<input type='text' value={"Lio Giladi"} disabled />
-				<button className={styles.filled}>{"Edit"}</button>
+				<input
+					type='text'
+					value={changingUserName}
+					onChange={onUserNameChange}
+					className={styles[`user-name-input-${isEditActive ? "save" : "edit"}`]}
+					disabled={!isEditActive}
+				/>
+				<button className={styles.filled} onClick={onEditbuttonClick}>
+					{isEditActive ? "Save" : "Edit"}
+				</button>
 			</section>
 
 			<p>{`Alive for ${daysAlive} days`}</p>
