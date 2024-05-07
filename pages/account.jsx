f97c8cbf8781e6293a7e123fa388ea6fd8e468e1
alonpeled44 <alon.orli44@gmail.com>
2024-05-07 11:09:56 +0300

    Add static responsive layout

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,5 +1,21 @@
 import styles from "@/styles/pages/account.module.scss";
-
 export default function Account() {
-	return <></>;
+	const daysAlive = 9;
+
+	return (
+		<main id={styles.main}>
+			<label for='profilePicInput' className={styles["profile-pic-input"]}>
+				<img src='/lio_img.png' alt="user's photo" />{" "}
+			</label>
+
+			<input type='file' id='profilePicInput' />
+
+			<section>
+				<input type='text' value={"Lio Giladi"} disabled />
+				<button className={styles.filled}>{"Edit"}</button>
+			</section>
+
+			<p>{`Alive for ${daysAlive} days`}</p>
+		</main>
+	);
 }
