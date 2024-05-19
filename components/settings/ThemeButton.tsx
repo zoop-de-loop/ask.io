d4b333d925ba71c90944e62367c90b36f13ce3d0
alonpeled44 <alon.orli44@gmail.com>
2024-05-19 11:49:11 +0300

    Redo setTheme if

--- components/settings/ThemeButton.tsx
+++ components/settings/ThemeButton.tsx
@@ -6,13 +6,11 @@ export default function ThemeButton() {
 	const { theme, setTheme } = useContext(ThemeContext);
 
 	const ToggleTheme = () => {
-		if(setTheme){
-			setTheme((prev) => {
-				const newValue = prev === "bright" ? "dark" : "bright";
-				localStorage.setItem("theme", newValue);
-				return newValue;
-			});
-		}
+		setTheme?.((prev) => {
+			const newValue = prev === "bright" ? "dark" : "bright";
+			localStorage.setItem("theme", newValue);
+			return newValue;
+		});
 	};
 
 	return <Button onClick={ToggleTheme}>{`${theme} theme`}</Button>;
