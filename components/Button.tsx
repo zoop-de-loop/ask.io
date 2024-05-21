1b9277e233cfdb4f833970333049f91a27e7c857
alonpeled44 <alon.orli44@gmail.com>
2024-05-21 14:37:11 +0300

    Add all button attributes to type

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,9 @@
 import React, { PropsWithChildren } from "react";
 import styles from "../styles/components/button.module.scss";
 
-interface Props {
-	onClick(): void;
-}
+interface Props extends PropsWithChildren, React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export default function Button({ children, ...rest }: PropsWithChildren<Props>) {
+export default function Button({ children, ...rest }: Props) {
 	return (
 		<button className={styles.button} {...rest}>
 			{children}
