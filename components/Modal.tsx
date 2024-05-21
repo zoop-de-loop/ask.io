423edc894632c9346e3464926fda575edb1b775b
alonpeled44 <alon.orli44@gmail.com>
2024-05-21 13:15:08 +0300

    Change if chain

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,33 +4,30 @@ import styles from "../styles/components/modal.module.scss";
 interface Props {
 	isOpen: boolean;
 	setIsOpen?: React.Dispatch<React.SetStateAction<boolean | null>>;
-  }
+}
 
-const Modal = ({ children, isOpen, setIsOpen }:PropsWithChildren<Props>) => {
+const Modal = ({ children, isOpen, setIsOpen }: PropsWithChildren<Props>) => {
 	const ref = useRef<HTMLDialogElement>(null);
 
 	useEffect(() => {
 		if (isOpen) {
-			if(ref.current){
-				ref.current.showModal();
-			}
+			ref.current?.showModal();
 		}
-
 	}, [isOpen]);
 
 	const handleBackgroundClicked = (event: React.MouseEvent) => {
 		if (event.target === ref.current) {
-			if(setIsOpen){
-				setIsOpen(false)
-			};
+			if (setIsOpen) {
+				setIsOpen(false);
+			}
 		}
 	};
 
 	const handleButtonClicked = (event: React.KeyboardEvent) => {
 		if (event.key === "Escape") {
-			if(setIsOpen){
-				setIsOpen(false)
-			};
+			if (setIsOpen) {
+				setIsOpen(false);
+			}
 		}
 	};
 
@@ -41,6 +38,6 @@ const Modal = ({ children, isOpen, setIsOpen }:PropsWithChildren<Props>) => {
 			{children}
 		</dialog>
 	);
-}
+};
 
 export default Modal;
